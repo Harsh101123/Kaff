44cfee502b242757d3b70ed97986ad9786d8e3b0
Harsh Neet <harshneet1011@gmail.com>
2025-08-19 17:14:41 +0530

    Updated code for new deployment

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -5,7 +5,7 @@ function HomeBanner() {
   return (
     <>
       <div className="banner_sec">
-        <img src="../../../public/discover-your-perfect-brew.webp" alt="Kaff Banner" />
+        <img src="/discover-your-perfect-brew.webp" alt="Kaff Banner" />
         <div className="container">
           <div className="banner_content">
             <h1>Reinvent Your Kitchen</h1>
